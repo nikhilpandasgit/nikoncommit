a2e3e5c08da0ebde454f3aa5892f35d254c600db
nikhilpandasgit <pandanikhil0408@gmail.com>
2025-05-15 23:29:55 +0530

    didnt work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const isIOS = typeof window !== 'undefined' && /iPhone|iPad|iPod/.test(navigator.userAgent);
 
   return (
     <>
@@ -27,8 +26,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <AnimatePresence mode="wait" initial={false}>
           <motion.div
             key={router.asPath}
-            initial={isIOS ? false : { opacity: 0, y: -10 }}
-            animate={isIOS ? {} : { opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -15 }}
             transition={{ duration: 0.4, ease: 'easeInOut' }}
           >
